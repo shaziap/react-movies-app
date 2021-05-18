085037ec4aa313df092490417565cabfefeb1db5
shaziap <shaiz.sit@gmail.com>
2021-05-18 23:24:24 +0530

    “Added 'required' field validation in username control in login form”

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -10,6 +10,7 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
+import FormHelperText from '@material-ui/core/FormHelperText';
 const customStyles = {
     content: {
         top: '50%',
@@ -34,9 +35,10 @@ class Header extends Component {
     constructor() {
         super();
         this.state = {
-
             modalIsOpen: false,
-            value: 0
+            value: 0,
+            usernameRequired: "dispNone",
+            username: ""
         }
     }
     openModalHandler = () => {
@@ -48,6 +50,13 @@ class Header extends Component {
     }
     tabChangeHandler = (event, value) => {
         this.setState({ value });
+    }
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+    }
+
+    inputUsernameChangeHandler = (e) => {
+        this.setState({ username: e.target.value });
     }
     render() {
         return (
@@ -75,7 +84,10 @@ class Header extends Component {
                         <TabContainer>
                             <FormControl required>
                                 <InputLabel htmlFor="username">Username</InputLabel>
-                                <Input id="username" type="text" />
+                                <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameChangeHandler} />
+                                <FormHelperText className={this.state.usernameRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
                             </FormControl>
                             <br /><br />
                             <FormControl required>
@@ -83,7 +95,7 @@ class Header extends Component {
                                 <Input id="password" type="password" />
                             </FormControl>
                             <br /><br />
-                            <Button variant="contained" color="primary">LOGIN</Button>
+                            <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
                         </TabContainer>
                     }
                 </Modal>
