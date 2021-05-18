c611501d8ed4cc5b0bafacefc3b9607c7c121951
shaziap <shaiz.sit@gmail.com>
2021-05-18 23:12:23 +0530

    “Added PropTypes for TabContainer and added a check for state value when displaying login TabContainer”

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
+import PropTypes from 'prop-types';
 const customStyles = {
     content: {
         top: '50%',
@@ -25,6 +26,9 @@ const TabContainer = function (props) {
             {props.children}
         </Typography>
     )
+}
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
 }
 class Header extends Component {
     constructor() {
@@ -63,23 +67,25 @@ class Header extends Component {
                     contentLabel="Login"
                     onRequestClose={this.closeModalHandler}
                     style={customStyles}>
-                  <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login" />
                         <Tab label="Register" />
                     </Tabs>
-                    <TabContainer>
-                        <FormControl required>
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input id="username" type="text" />
-                        </FormControl>
-                        <br /><br />
-                        <FormControl required>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input id="password" type="password" />
-                        </FormControl>
-                        <br /><br />
-                        <Button variant="contained" color="primary">LOGIN</Button>
-                    </TabContainer>
+                    {this.state.value === 0 &&
+                        <TabContainer>
+                            <FormControl required>
+                                <InputLabel htmlFor="username">Username</InputLabel>
+                                <Input id="username" type="text" />
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input id="password" type="password" />
+                            </FormControl>
+                            <br /><br />
+                            <Button variant="contained" color="primary">LOGIN</Button>
+                        </TabContainer>
+                    }
                 </Modal>
 
             </div>
